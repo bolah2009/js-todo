d9308b716d55dc721dc5b26b462c798fc2f70d2d
Bola Ahmed Buari <bola_ahmed2002@yahoo.com>
2019-10-09 16:09:24 +0100

    Update fragments module to set other attributes
    The fragment module initailly have the class attribute set, this will help other attributes to be set when creating a new fragment.

--- src/modules/views/fragments.js
+++ src/modules/views/fragments.js
@@ -1,11 +1,18 @@
 const createFragment = (props = {}) => {
-  const { classNames = '', element = 'div', html = '' } = props;
+  const {
+    classNames = '', element = 'div', html = '', otherAttributes = [],
+  } = props;
   const documentFragment = document.createDocumentFragment();
   const newElement = document.createElement(element);
   if (classNames.length > 0) {
     newElement.classList.add(...classNames.split(' '));
   }
   newElement.innerHTML = html;
+  if (otherAttributes.length) {
+    otherAttributes.forEach(({ name, value }) => {
+      newElement.setAttribute(name, value);
+    });
+  }
   documentFragment.appendChild(newElement);
   return documentFragment;
 };
