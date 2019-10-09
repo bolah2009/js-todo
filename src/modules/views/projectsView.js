e0cef888d739c024496a9190d27c501f62db72ac
Bola Ahmed Buari <bola_ahmed2002@yahoo.com>
2019-10-09 16:33:52 +0100

    update project view with functionality
    Project view now have a add button for creating new project and update the view without refreshing the page when adding new project

--- src/modules/views/projectsView.js
+++ src/modules/views/projectsView.js
@@ -1,37 +1,59 @@
 import createFragment from './fragments';
+import projectModel from '../models/projectModel';
+
+const projectListItemFragment = (project, index) => {
+  const classNames = 'project-list-item';
+  const element = 'li';
+  const html = `<button data-id="project-${index}" type="button" class="button">
+                   ${project}
+                </button>`;
+  return createFragment({ classNames, element, html });
+};
+
+const appendProjectListItem = (list) => {
+  const append = (project, index) => {
+    const listItem = projectListItemFragment(project, index);
+    return list.appendChild(listItem);
+  };
+  return append;
+};
+
+const projectListFragment = (projects = []) => {
+  const classNames = 'project-list';
+  const element = 'ul';
+  const html = '';
+  const projectList = createFragment({ classNames, element, html }).firstElementChild;
+  projects.forEach(appendProjectListItem(projectList));
+  return projectList;
+};
 
 const projectNavBarFragment = () => {
+  const projects = projectModel.store;
   const navBarProps = () => {
     const classNames = 'projects-modal d-none d-flex-md col';
     const element = 'nav';
     const html = `
                 <div class="projects">
                 <h4 class="project-title">projects</h4>
-                <ul class="project-list">
-                <li class="project-list-item">
-                    <button type="button" class="button">
-                    Personal
-                    </button>
-                </li>
-                <li class="project-list-item">
-                    <button type="button" class="button">
-                    Work
-                    </button>
-                </li>
-                </ul>
-                <button class="add-project button">
-                <svg class="svg-add" viewBox="0 0 24 24">
-                    <path
-                    fill="#000000"
-                    d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z"
-                    />
-                </svg>
+                <button data-action="add-project" class="add-project button">
+                  <svg data-action="add-project" class="svg-add" viewBox="0 0 24 24">
+                    <path data-action="add-project" fill="#000000" d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z"/>
+                  </svg>
                 </button>
                 </div>`;
     return { classNames, element, html };
   };
-  return createFragment(navBarProps());
+
+  const navBarFragment = createFragment(navBarProps());
+
+  const parentNode = navBarFragment.querySelector('.projects');
+  const addProjectButton = navBarFragment.querySelector('.add-project.button');
+
+  const projectListElement = projectListFragment(projects);
+
+  parentNode.insertBefore(projectListElement, addProjectButton);
+
+  return navBarFragment;
 };
 
-
-export default projectNavBarFragment;
+export { projectNavBarFragment, appendProjectListItem };
